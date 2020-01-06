92dd6f2b4166b7702175828179080c07534785a9
Christopher Smith <christophersmith@users.noreply.github.com>
2020-01-06 12:08:17 -0500

    modified songplays.songplay_id to be a serial instead of a int

--- etl.py
+++ etl.py
@@ -98,7 +98,7 @@ def process_log_file(cur, filepath):
                 songid, artistid = None, None
 
             # insert songplay record
-            songplay_data = (index, row.ts, row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent)
+            songplay_data = (row.ts, row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent)
             try:
                 cur.execute(songplay_table_insert, songplay_data)
             except psycopg2.Error as e:
--- sql_queries.py
+++ sql_queries.py
@@ -9,7 +9,7 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 # CREATE TABLES
 
 songplay_table_create = ("CREATE TABLE songplays ( \
-songplay_id int PRIMARY KEY, \
+songplay_id serial PRIMARY KEY, \
 start_time timestamp NOT NULL, \
 user_id int NOT NULL, \
 level varchar NOT NULL, \
@@ -56,11 +56,11 @@ weekday int NOT NULL \
 
 # INSERT RECORDS
 
-songplay_table_insert = ("INSERT INTO songplays (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) \
-VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT DO NOTHING")
+songplay_table_insert = ("INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) \
+VALUES(%s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT DO NOTHING")
 
 user_table_insert = ("INSERT INTO users (user_id, first_name, last_name, gender, level) VALUES(%s, %s, %s, %s, %s) \
-ON CONFLICT DO UPDATE level = EXCLUDED.level")
+ON CONFLICT (user_id) DO UPDATE SET level = EXCLUDED.level")
 
 song_table_insert = ("INSERT INTO songs (song_id, title, artist_id, year, duration) VALUES(%s, %s, %s, %s, %s) ON CONFLICT DO NOTHING")
 
